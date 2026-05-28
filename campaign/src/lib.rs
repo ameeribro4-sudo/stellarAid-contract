0f8cf5952ae7d75883a91b9f6ea0c72f61394336
wumibals <omowumibalogun81@gmail.com>
2026-05-28 15:28:37 +0100

    feat(campaign): add VERSION constant and version() query function

--- campaign/src/lib.rs
+++ campaign/src/lib.rs
@@ -1,6 +1,8 @@
 #![no_std]
 use soroban_sdk::{contract, contractimpl, Env};
 
+pub const VERSION: u32 = 1;
+
 #[contract]
 pub struct CampaignContract;
 
@@ -8,5 +10,9 @@ pub struct CampaignContract;
 impl CampaignContract {
     pub fn hello(env: Env) -> soroban_sdk::Symbol {
         soroban_sdk::Symbol::new(&env, "campaign")
+    }
+
+    pub fn version() -> u32 {
+        VERSION
     }
 }
